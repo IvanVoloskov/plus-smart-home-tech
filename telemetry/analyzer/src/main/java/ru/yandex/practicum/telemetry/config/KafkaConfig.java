dfd8858dee61ecca2b4e76a5636eb00360ebde19
Voloskov Ivan <voloskov.ivan03@mail.ru>
2026-06-03 15:30:46 +0300

    fix some bugs

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/config/KafkaConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/config/KafkaConfig.java
@@ -26,7 +26,7 @@ public class KafkaConfig {
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer-snapshot-group");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SensorsSnapshotDeserializer.class);
-        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         return new KafkaConsumer<>(config);
     }
 
