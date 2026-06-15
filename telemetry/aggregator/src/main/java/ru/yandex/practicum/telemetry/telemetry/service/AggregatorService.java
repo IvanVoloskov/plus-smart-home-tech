54772260af9c26ac4aeed1ebeb3fd51678847fb0
Voloskov Ivan <voloskov.ivan03@mail.ru>
2026-06-15 14:30:53 +0300

    fix: aggregator logic with oldstate

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/telemetry/service/AggregatorService.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/telemetry/service/AggregatorService.java
@@ -27,7 +27,7 @@ public class AggregatorService {
         SensorStateAvro oldState = snapshot.getSensorsState().get(event.getId());
 
         if (oldState != null) {
-            if (!oldState.getTimestamp().isBefore(event.getTimestamp()) ||
+            if (oldState.getTimestamp().isAfter(event.getTimestamp()) ||
                     oldState.getData().equals(event.getPayload())) {
                 return Optional.empty();
             }
